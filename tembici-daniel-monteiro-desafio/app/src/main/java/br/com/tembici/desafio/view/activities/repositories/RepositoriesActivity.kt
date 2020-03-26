25b2685ba40e34b4ec12977be7554533d4ac45c3
Daniel Monteiro <daniell.monteirosousa@gmail.com>
2020-03-26 09:41:53 -0300

    Lista de Repositórios: 	Mensagem de erro em caso de problema com internet ou outro erro na API; 	Progress na tela em vez de um dialog progress; 	Melhoria do foco da lista quando carrega novas páginas;

--- tembici-daniel-monteiro-desafio/app/src/main/java/br/com/tembici/desafio/view/activities/repositories/RepositoriesActivity.kt
+++ tembici-daniel-monteiro-desafio/app/src/main/java/br/com/tembici/desafio/view/activities/repositories/RepositoriesActivity.kt
@@ -2,6 +2,7 @@ package br.com.tembici.desafio.view.activities.repositories
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.AbsListView
 import br.com.tembici.desafio.R
 import br.com.tembici.desafio.model.ItemRepository
@@ -24,35 +25,10 @@ class RepositoriesActivity : BaseActivity(), AbsListView.OnScrollListener,
     RepositoriesPresenterView.ViewReturnRepositories {
 
     var listAllRepositories: ArrayList<ItemRepository>? = ArrayList<ItemRepository>()
-
-    override fun showProgressLoading() {
-        showProgressDialog()
-    }
-
-    override fun hideProgressLoading() {
-        dismissProgressDailog()
-    }
-
-    override fun onSuccess(responseList: Response<RepositoriesReturn>) {
-        val returnListRepositories = responseList.body()!!
-        this.listAllRepositories!!.addAll(returnListRepositories.items)
-        setAdapterList(this.listAllRepositories!!)
-    }
-
-    override fun onError() {
-
-    }
-
-    override fun intetnetError() {
-
-    }
-
-    override fun checkInternet(): Boolean {
-        return Connection.isConexaoInternet(this)
-    }
+    var qtdListaCurrentePage: Int = 0
 
     private var visibleThreshold = 5
-    private var currentPage = 1
+    private var currentPage = 0
     private var previousTotal = 0
     private var loading = false
     var isReloadPage = false
@@ -64,19 +40,16 @@ class RepositoriesActivity : BaseActivity(), AbsListView.OnScrollListener,
         setUpInicialConfigActivity()
 
         lvRepositories.setOnScrollListener(this)
-
-        callRepositoriesRequest()
     }
 
     private fun setAdapterList(listItems: ArrayList<ItemRepository>) {
+        lvRepositories.visibility = View.VISIBLE
         val adapter = RepositoriesAdapter(this, listItems)
         lvRepositories.adapter = adapter
 
-        // Revisar setar para o final toda vez que recarregar para uma nova página
-
-//        if (isReloadPage) {
-//            lvRepositories.setSelection(adapter.count - 1);
-//        }
+        if (isReloadPage && currentPage > 1) {
+            lvRepositories.setSelection((listAllRepositories!!.size - qtdListaCurrentePage - 5))
+        }
     }
 
     private fun callRepositoriesRequest() {
@@ -84,36 +57,56 @@ class RepositoriesActivity : BaseActivity(), AbsListView.OnScrollListener,
         repositoryPresenterData.getRepositories(this.currentPage)
     }
 
-    override fun onScroll(p0: AbsListView?, firstItemVisible: Int, visibleItemCounting: Int, totalItemCount: Int) {
-//        if (loading) {
-//            if (totalItemCount > previousTotal) {
-//                loading = false
-//                previousTotal = totalItemCount
-//                currentPage ++
-//            }
-//        }
-//
-//        if (!loading && (totalItemCount - visibleItemCounting) <= (firstItemVisible - visibleThreshold)) {
-//            callRepositoriesRequest()
-//            loading = true
-//        }
+    override fun onScroll(p0: AbsListView?, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
 
-        Log.d("Teste", "TESTE")
-
-        if ((firstItemVisible+visibleItemCounting) == totalItemCount) {
-            currentPage += 1
-            if (currentPage > 1) {
-//                isReloadPage = true
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false
+                isReloadPage = false
+                previousTotal = totalItemCount
             }
-            callRepositoriesRequest()
         }
 
+        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+            currentPage += 1
+            callRepositoriesRequest()
+            loading = true
+            isReloadPage = true
+        }
 
     }
 
     override fun onScrollStateChanged(p0: AbsListView?, p1: Int) {
-        showMessageToast(this, R.string.msg_click, true)
+//        showMessageToast(this, R.string.msg_click, true)
     }
+
+    override fun showProgressLoading() {
+        pbLoading.visibility = View.VISIBLE
+    }
+
+    override fun hideProgressLoading() {
+        pbLoading.visibility = View.GONE
+    }
+
+    override fun onSuccess(responseList: Response<RepositoriesReturn>) {
+        val returnListRepositories = responseList.body()!!
+        qtdListaCurrentePage = responseList.body()!!.items.size
+        this.listAllRepositories!!.addAll(returnListRepositories.items)
+        setAdapterList(this.listAllRepositories!!)
+    }
+
+    override fun onError() {
+        showMessage(this, R.string.title_error, R.string.msg_error, 0, false)
+    }
+
+    override fun intetnetError() {
+        showMessage(this, R.string.title_error, R.string.msg_error, 0, false)
+    }
+
+    override fun checkInternet(): Boolean {
+        return Connection.isConexaoInternet(this)
+    }
+
 
 
 }
